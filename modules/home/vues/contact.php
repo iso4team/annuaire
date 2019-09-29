48c5cf9189c86fbfcc1821daccde022c71d63b2e
Daouda Manga <macksoft2@gmail.com>
2019-09-29 08:21:08 +0000

    Delete engregistrement Product, Customer, etc.

--- modules/home/vues/contact.php
+++ modules/home/vues/contact.php
@@ -6,90 +6,41 @@
         </div>
         <div class="row">
             <?= $texte; ?>
+            <div class="col-lg-6 col-md-6 col-xs-12">
+                Forumulaire de contact
+                <form id="contact-form" action="#" method="get">
+                              <div class="col-md-6 col-sm-6">
+                                   <input type="text" class="form-control" name="name" placeholder="Nom Complet" required="">
+                              </div>
+                              <div class="col-md-6 col-sm-6">
+                                   <input type="email" class="form-control" name="email" placeholder="Email" required="">
+                              </div>
+                              <div class="col-md-12 col-sm-12">
+                                   <textarea class="form-control" rows="5" name="message" placeholder="Message" required=""></textarea>
+                              </div>
+                              <div class="col-md-offset-8 col-md-4 col-sm-offset-6 col-sm-6">
+                                   <button id="submit" type="submit" class="form-control" name="submit">Envoyer</button>
+                              </div>
+                        </form>
+            </div>
+            <div class="col-lg-6 col-md-6 col-xs-12">
+                Adresse
+                <iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=Dakar&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+                    
+                </iframe>
+            </div>
             
-            <div class="section vc_row wpb_row vc_row-fluid "><div class="row">
-	<div class="vc_col-sm-12 large-12 columns wpb_column vc_column_container ">
-		<div class="wpb_wrapper">
-			
-<div class="vc_separator wpb_content_element">
-
-<div class="sub-title">JOBX</div><br>
-<h2 class="title">
-Contactez-nous</h2>
-
-</div>
-
-
-<div class="vc_row wpb_row vc_inner vc_row-fluid"><div class="wpb_column vc_column_container vc_col-sm-12 large-12 columns"><div class="wpb_wrapper"><div class="wpb_gmaps_widget wpb_content_element">
-	<div class="wpb_wrapper">
-	<div class="wpb_map_wraper">
-		
-            <div class="mapouter"><div class="gmap_canvas"><iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=Dakar&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://usave.co.uk">usave</a></div><style>.mapouter{position:relative;text-align:right;height:500px;width:600px;}.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:600px;}</style></div>
-        </div>
-	</div>
-<div class="vc_empty_space" style="height: 32px"><span class="vc_empty_space_inner"></span></div>
-</div></div></div><div class="vc_row wpb_row vc_inner vc_row-fluid"><div class="wpb_column vc_column_container vc_col-sm-6 large-6 columns"><div class="wpb_wrapper">
-	<div class="wpb_text_column wpb_content_element">
-		<div class="wpb_wrapper">
-			<h2 style="text-align: left;">Informations pratiques</h2>
-<p style="text-align: left;">Téléphone : 03 44 47 14 30<br>
-Email : dragimmo@wanadoo.fr<br>
-Adresse : 26 Avenue Salvador Allende, 60 000 Beauvais</p>
-
-		</div>
-	</div>
-<div class="vc_empty_space" style="height: 32px"><span class="vc_empty_space_inner"></span></div>
-</div></div><div class="wpb_column vc_column_container vc_col-sm-6 large-6 columns"><div class="wpb_wrapper">
-	<div class="wpb_text_column wpb_content_element">
-		<div class="wpb_wrapper">
-			<h2 style="text-align: right;">Horaires d’ouverture</h2>
-<p style="text-align: right;">Nous sommes à votre écoute<br>
-Du Lundi au Vendredi<br>
-De 9h00 à 12h30 et de 14h00 à 18h00</p>
-
-		</div>
-	</div>
-<div class="vc_empty_space" style="height: 32px"><span class="vc_empty_space_inner"></span></div>
-</div></div></div>
-		</div> 
-	</div> 
-
-	<div class="vc_col-sm-12 large-12 columns wpb_column vc_column_container ">
-		<div class="wpb_wrapper">
-			
-	<div class="wpb_text_column wpb_content_element">
-		<div class="wpb_wrapper">
-			<h2 style="text-align: center;">Formulaire de contact</h2>
-<p style="text-align: center;"><b>Vous souhaitez investir dans un projet immobilier professionnel ? Acheter ou louer un local pour votre entreprise ?<br>
-</b>Remplissez le formulaire de contact ci-dessous, nous vous recontacterons dans les plus brefs délais.</p>
-
-		</div>
-	</div>
-<div class="vc_row wpb_row vc_inner vc_row-fluid"><div class="wpb_column vc_column_container vc_col-sm-3 large-3 columns"><div class="wpb_wrapper"><div class="vc_empty_space" style="height: 32px"><span class="vc_empty_space_inner"></span></div>
-</div></div><div class="wpb_column vc_column_container vc_col-sm-6 large-6 columns"><div class="wpb_wrapper"><div role="form" class="wpcf7" id="wpcf7-f4-p67-o1" dir="ltr" lang="en-US">
-<div class="screen-reader-response"></div>
-<form name="" action="/contact/#wpcf7-f4-p67-o1" method="post" class="wpcf7-form" novalidate="novalidate">
-<div style="display: none;">
-<input type="hidden" name="_wpcf7" value="4">
-<input type="hidden" name="_wpcf7_version" value="4.2.1">
-<input type="hidden" name="_wpcf7_locale" value="en_US">
-<input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f4-p67-o1">
-<input type="hidden" name="_wpnonce" value="509b8803c1">
-</div>
-<p>Nom/Prénom*<br><span class="wpcf7-form-control-wrap your-name"><input type="text" name="your-name" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false"></span> </p>
-<p>Société<br><span class="wpcf7-form-control-wrap societe"><input type="text" name="societe" value="" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false"></span> </p>
-<p>Adresse<br><span class="wpcf7-form-control-wrap Adresse"><input type="text" name="Adresse" value="" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false"></span> </p>
-<p>Téléphone*<br><span class="wpcf7-form-control-wrap text-226"><input type="text" name="text-226" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false"></span> </p>
-<p>Mail<br><span class="wpcf7-form-control-wrap your-email"><input type="email" name="your-email" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-email" aria-invalid="false"></span> </p>
-<p>Votre message<br><span class="wpcf7-form-control-wrap your-message"><textarea name="your-message" cols="40" rows="10" class="wpcf7-form-control wpcf7-textarea" aria-invalid="false"></textarea></span> </p>
-<p></p><center>
-<p><input type="submit" value="Envoyer" class="wpcf7-form-control wpcf7-submit"><img class="ajax-loader" src="https://dragimmo.com/wordpress/wp-content/plugins/contact-form-7/images/ajax-loader.gif" alt="Envoi en cours ..." style="visibility: hidden;"></p>
-<p></p></center><p></p>
-<div class="wpcf7-response-output wpcf7-display-none"></div></form></div></div></div><div class="wpb_column vc_column_container vc_col-sm-3 large-3 columns"><div class="wpb_wrapper"><div class="vc_empty_space" style="height: 32px"><span class="vc_empty_space_inner"></span></div>
-</div></div></div>
-		</div> 
-	</div> 
-</div></div>
-        </div>  
-    </div> 
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            </div> 
 </section>
